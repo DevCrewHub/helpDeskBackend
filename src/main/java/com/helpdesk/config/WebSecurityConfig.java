97ec1c1b12f8089981a8905dc3af803d857c1470
MahakAgrawal02 <agrawalmahak0206@gmail.com>
2025-07-15 19:33:25 +0530

    Online Database setup

--- src/main/java/com/helpdesk/config/WebSecurityConfig.java
+++ src/main/java/com/helpdesk/config/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:5173"));
+		config.setAllowedOrigins(List.of("http://localhost:5173", "https://helpdesk-support-system.netlify.app"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
